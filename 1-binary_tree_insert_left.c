47e79de5bc6ef515ec52efa09a81df420f9f8da6
Magdy156 <magdyhamdy156@gmail.com>
2023-09-05 18:40:03 +0300

    task 1 mdoified

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,8 +9,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
 
+	if (!parent)
+		return (NULL);
 	newNode = binary_tree_node(parent, value);
-	if (!parent || !newNode)
+	if (!newNode)
 		return (NULL);
 
 	if (parent->left)
