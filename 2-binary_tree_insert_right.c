3eab9da3015a89c94d9cea97e21cdba0e57dcfd0
Magdy156 <magdyhamdy156@gmail.com>
2023-09-06 00:02:14 +0300

    task2 betty modification

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,11 +1,12 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_right -  inserts a node as the right-child of another node
+ * binary_tree_insert_right - inserts a node as the right-child of another node
  * @parent: node parent
  * @value: value of the node
  * Return: the new node
 */
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent,
+										int value)
 {
 	binary_tree_t *newNode;
 
