a2844df0df771fd696f5dbe75029bd77323cb888
Magdy156 <magdyhamdy156@gmail.com>
2023-09-05 23:59:25 +0300

    task 2

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right -  inserts a node as the right-child of another node
+ * @parent: node parent
+ * @value: value of the node
+ * Return: the new node
+*/
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode;
+
+	if (!parent)
+		return (NULL);
+	newNode = binary_tree_node(parent, value);
+	if (!newNode)
+		return (NULL);
+
+	if (parent->right)
+	{
+		newNode->right = parent->right;
+		parent->right->parent = newNode;
+	}
+	parent->right = newNode;
+	return (newNode);
+}
